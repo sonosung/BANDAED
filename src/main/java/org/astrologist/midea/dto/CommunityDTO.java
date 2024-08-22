9570cd1152770f3da8b6e67280a20c58362eb415
manic-066g <yoo75583@gmail.com>
2024-08-22 19:25:26 +0900

    2024.08.22 시간 수정 김별

--- src/main/java/org/astrologist/midea/dto/CommunityDTO.java
+++ src/main/java/org/astrologist/midea/dto/CommunityDTO.java
@@ -1,6 +1,9 @@
 package org.astrologist.midea.dto;
 
 import lombok.*;
+import org.astrologist.midea.entity.Community;
+
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @Setter
@@ -14,6 +17,15 @@ public class CommunityDTO {
     private String content;
     private String timestamp;
     private String subcategory;  // Community 엔티티의 Subcategory 열거형을 사용
+
+    public CommunityDTO(Community community) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        this.timestamp = community.getTimestamp().format(formatter);
+        this.userId = community.getUser().getId();
+        this.composer = community.getComposer();
+        this.content = community.getContent();
+        this.subcategory = community.getSubcategory().name();  // Subcategory 열거형의 이름을 문자열로 변환
+    }
 
     @Override
     public String toString() {
